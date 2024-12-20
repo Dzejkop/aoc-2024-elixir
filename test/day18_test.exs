37b819556b2fc78bbc8074d17a22da33e273bbc7
Dzejkop <jakubtrad@gmail.com>
2024-12-20 12:18:30 +0100

    Day 18 part two

--- test/day18_test.exs
+++ test/day18_test.exs
@@ -97,4 +97,46 @@ defmodule Day18Test do
 
     assert 22 == length(path) - 1
   end
+
+  test "example - part two" do
+    content = """
+    5,4
+    4,2
+    4,5
+    3,0
+    2,1
+    6,3
+    2,4
+    1,5
+    0,6
+    3,3
+    2,6
+    5,1
+    1,2
+    5,5
+    2,5
+    6,5
+    1,4
+    0,4
+    6,4
+    1,1
+    6,1
+    1,0
+    0,5
+    1,6
+    2,0
+    """
+
+    bytes = Day18.parse(content) |> Enum.take(12)
+    map = Day18.simulate(bytes, {7, 7})
+
+    IO.puts("")
+    GridMap.print(map)
+
+    graph = Day18.map_to_graph(map)
+
+    path = Graph.dijkstra(graph, {0, 0}, {6, 6})
+
+    assert 22 == length(path) - 1
+  end
 end
